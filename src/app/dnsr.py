8836856f58d60e8a16f2469753039b8a1d9ab74b
Jesus Romero <jromero@theworkshop.com>
2025-01-02 16:57:16 +0100

    My bad

--- src/app/dnsr.py
+++ src/app/dnsr.py
@@ -201,14 +201,14 @@ async def dnsr_created(spec, patch, logger, **_):
         update_status(patch, Statuses.SYNCED, f"N/A")
 
 
-# # Validating Admission Webhook
-# @kopf.on.validate(CDR, operations=["CREATE", "UPDATE"], persistent=True)
-# async def uniquerecord(spec, **_):
-#     dyn_client = DynamicClient(kubernetes.client.ApiClient())
-#     all_dnsr = dyn_client.resources.get(kind=CDR).get()
-#     for dnsr in all_dnsr.attributes.items:
-#         if (dnsr.spec.record.rstrip(".") == spec.get("record").rstrip(".")) and (spec._src.get("metadata")["name"] != dnsr.get("metadata")["name"]):
-#             raise kopf.AdmissionError(f"Record must be unique. This record {spec.get('record')} is already being managed by {dnsr.metadata.namespace}/{dnsr.metadata.name}.", code=409)
+# Validating Admission Webhook
+@kopf.on.validate(CDR, operations=["CREATE", "UPDATE"], persistent=True)
+async def uniquerecord(spec, **_):
+    dyn_client = DynamicClient(kubernetes.client.ApiClient())
+    all_dnsr = dyn_client.resources.get(kind=CDR).get()
+    for dnsr in all_dnsr.attributes.items:
+        if (dnsr.spec.record.rstrip(".") == spec.get("record").rstrip(".")) and (spec._src.get("metadata")["name"] != dnsr.get("metadata")["name"]):
+            raise kopf.AdmissionError(f"Record must be unique. This record {spec.get('record')} is already being managed by {dnsr.metadata.namespace}/{dnsr.metadata.name}.", code=409)
 
 
 # TODO decide if we want to trigguer by timer
